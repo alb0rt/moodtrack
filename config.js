0c9d19f0a8a88253449bc0d28184f63f5155b556
alb0rt <albert.xi.chen@gmail.com>
2014-09-14 23:19:42 -0700

    worker

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ config.twilio = {};
 
 config.twilio.sid = 'ACd9736e36084695b8fe325153a9c36b8a';
 config.twilio.key = '29971904e99a605f82c6733ab16e8fee';
+config.twilio.number = '+12038899300';
 config.twilio.smsWebhook = 'https://damp-brushlands-4755.herokuapp.com/sms';
 config.twilio.voiceWebhook = 'https://damp-brushlands-4755.herokuapp.com/vote/voice';
 config.disableTwilioSigCheck = false;
--- worker.js
+++ worker.js
@@ -1,5 +1,23 @@
-function sayHello() {
-	console.log("Hello");
+var twilio = require('twilio');
+var config = require('./config');
+var client = new twilio.RestClient(config.twilio.sid, config.twilio.key);
+
+function sendReminder() {
+	client.sms.messages.create({
+		to : '+12036450330',
+		from : config.twilio.number,
+		body : 'How was work today?'
+	}, function(error, message) {
+		if(!error) {
+			console.log("Message successfully sent with SID: ");
+			console.log(message.sid);
+
+			console.log("Message sent on: ");
+			console.log(message.dateCreated);
+		} else {
+			console.log("Error sending message");
+		}
+	});
 }
 
-sayHello();
+sendReminder();
