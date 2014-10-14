12d8dff2c97975b99c96608735b89bd451172b87
alb0rt <albert.xi.chen@gmail.com>
2014-10-13 21:34:05 -0700

    bugfix

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var twilio = require('twilio');
 var config = require('../config');
+var utils = require('./utils');
 
 /* GET home page. */
 router.get('/', function(req, res) {
