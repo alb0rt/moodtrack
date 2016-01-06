ee92a017b2433a32156e68375bc0f59be7056d4f
Albert <alb0rt@users.noreply.github.com>
2016-01-06 11:26:44 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {};
 config.twilio = {};
 
 config.twilio.sid = 'ACd9736e36084695b8fe325153a9c36b8a';
-config.twilio.key = '29971904e99a605f82c6733ab16e8fee';
+config.twilio.key = ;
 config.twilio.number = '+12038899300';
 config.twilio.smsWebhook = 'https://damp-brushlands-4755.herokuapp.com/sms';
 config.twilio.voiceWebhook = 'https://damp-brushlands-4755.herokuapp.com/vote/voice';
